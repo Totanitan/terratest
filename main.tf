a811f8b3405d5c5e4f959fefef971dbdfac25a68
ttnsheep <ttnsheep@gmail.com>
2022-03-05 21:57:20 +0900

    initial

---
+++ main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "vpc-handson"
+  }
+}
+
+resource "aws_subnet" "aws-tf-public-subnet" {
+    vpc_id           = aws_vpc.main.id
+    cidr_block      = "10.0.0.0/24"
+    tags = {
+        Name = "aws-tf-public-subnet"
+    }
+}
+
+resource "aws_subnet" "aws-tf-private-subnet" {
+    vpc_id           = aws_vpc.main.id
+    cidr_block      = "10.0.1.0/24"
+    tags = {
+        Name = "aws-tf-private-subnet"
+    }
+}
+
+resource "aws_internet_gateway" "aws-tf-igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "aws-tf-igw"
+  }
+}
+
+resource "aws_instance" "hello" {
+  ami = "ami-0701e21c502689c31"
+  instance_type = "t3.micro"
+  }
